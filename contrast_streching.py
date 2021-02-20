c1d38ecff67077bf67a09686741121356356ffba
Your Name <mashour365@gmail.com>
2021-02-20 04:54:50 +0200

    Added contrast stretching

---
+++ contrast_streching.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+# read image
+img = cv2.imread("Images/Bikesgray.jpg", cv2.IMREAD_COLOR)
+
+# normalize float versions
+norm_img1 = cv2.normalize(img, None, alpha=2, beta=4, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+norm_img2 = cv2.normalize(img, None, alpha=0, beta=1.2, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+
+# scale to uint8
+norm_img1 = (255 * norm_img1).astype(np.uint8)
+norm_img2 = np.clip(norm_img2, 0, 1)
+norm_img2 = (255 * norm_img2).astype(np.uint8)
+
+# write normalized output images
+cv2.imwrite("EditedImages/contrast1.jpg", norm_img1)
+cv2.imwrite("EditedImages/contrast2.jpg", norm_img2)
