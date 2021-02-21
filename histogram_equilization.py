67748a24a8ea98c880d4a34898ec7774bda15db7
Your Name <mashour365@gmail.com>
2021-02-21 02:42:27 +0200

    Update histo_eq.png and histogram_equilization.py

--- histogram_equilization.py
+++ histogram_equilization.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('wiki.jpg', 0)
+img = cv2.imread('Images/wiki.jpeg', 0)
 
 hist, bins = np.histogram(img.flatten(), 256, [0, 256])
 
@@ -14,3 +14,20 @@ plt.hist(img.flatten(), 256, [0, 256], color='r')
 plt.xlim([0, 256])
 plt.legend(('cdf', 'histogram'), loc='upper left')
 plt.show()
+cdf_m = np.ma.masked_equal(cdf, 0)
+cdf_m = (cdf_m - cdf_m.min()) * 255 / (cdf_m.max() - cdf_m.min())
+cdf = np.ma.filled(cdf_m, 0).astype('uint8')
+img2 = cdf[img]
+plt.imshow(img2)
+plt.show()
+
+
+# OR USING OPENCV
+def histo_eq():
+    img = cv2.imread('Images/wiki.jpeg', 0)
+    equ = cv2.equalizeHist(img)
+    res = np.hstack((img, equ))  # stacking images side-by-side
+    cv2.imwrite('EditedImages/histo_eq.png', res)
+
+
+histo_eq()
