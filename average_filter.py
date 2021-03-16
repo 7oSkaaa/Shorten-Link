51e51ed1ee5d60e363dadc1ae63cc14bab20148e
waelahmed99 <waelahme6599@gmail.com>
2021-03-16 21:58:54 +0200

    Optimizing complexity

--- average_filter.py
+++ average_filter.py
@@ -4,6 +4,8 @@ import numpy as np
 
 
 def average_filter(img):
+    """Average filter"""
+    '''Average filter'''
     # Obtain number of rows and columns
     # of the image
     m, n = img.shape
@@ -12,18 +14,28 @@ def average_filter(img):
     mask = np.full((3, 3), 2)
     mask = mask / 9
 
+    szn = 3
+    szm = 3
     # Convolve the 3X3 mask over the image
     img_new = np.zeros([m, n])
 
     for i in range(1, m - 1):
         for j in range(1, n - 1):
-            temp = img[i - 1, j - 1] * mask[0, 0] + img[i - 1, j] * mask[0, 1] + img[i - 1, j + 1] * mask[0, 2] + img[
-                i, j - 1] * mask[1, 0] + img[i, j] * mask[1, 1] + img[i, j + 1] * mask[1, 2] + img[i + 1, j - 1] * mask[
-                       2, 0] + img[i + 1, j] * mask[2, 1] + img[i + 1, j + 1] * mask[2, 2]
+            tmp = 0
+            for ii in range(-1, 2):
+                for jj in range(-1, 2):
+                    xx = i + ii
+                    yy = j + jj
+                    if xx < 0 or xx >= m:
+                        continue
+                    if yy < 0 or yy >= n:
+                        continue
+                    tmp += img[xx, yy]
+                    #print(ii + 1, jj + 1, mask[ii + 1, jj + 1], sep="  ")
 
-            img_new[i, j] = temp
+            img_new[i, j] = tmp / 9
+            #print(img_new[i, j], img[i, j], sep="   ")
 
-    img_new = img_new.astype(np.uint8)
     cv2.imwrite('EditedImages/mostly_low_pass_bike.jpg', img_new)
     return img_new
 
