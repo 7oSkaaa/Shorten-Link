0f51b94bb3ce987a1ef0fd3ca6722d941bad67a5
Ahmed Hossam <ahmed.7oskaa@gmail.com>
2022-04-09 19:23:26 +0200

    use cutomize options to chrome driver

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 load_dotenv()
 
+def create_driver():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
 
 def saveToFile(fileName, mode, data):
     with open(f'{fileName}', mode) as file:
