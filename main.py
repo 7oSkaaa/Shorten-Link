b5f3ac0cf10e9e64536d04701ac6a8a2a3f62e76
Ahmed Hossam <ahmed.7oskaa@gmail.com>
2022-04-09 19:24:12 +0200

    use cutomize options to chrome driver

--- main.py
+++ main.py
@@ -13,10 +13,13 @@ load_dotenv()
 
 def create_driver():
     chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(options=chrome_options)
+    chrome_options.add_argument("--disable-notifications")
+    chrome_options.add_argument("--disable-popup-blocking")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     return driver
 
 def saveToFile(fileName, mode, data):
