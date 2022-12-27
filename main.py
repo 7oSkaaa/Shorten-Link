70a686f8379ab87a51123769675aef2767d69cfb
7oSkaaa <ahmed.7oskaa@gmail.com>
2022-12-27 19:41:43 +0200

    refactor: naming the paramter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from Services.link_shorten import shortenLink
 from Helpers.colors import colors
-from Services.link_shorten import change_directory
+from Services.link_shorten import change_directory, shortenLink
 
 
 def saveToFile(fileName, mode, data, message):
@@ -24,9 +23,9 @@ def main():
     title = shorten_url_response["title"]
 
     # Save the links to the file
-    saveToFile("URLs.txt", "w", shorten_url, "Shorten URL")
-    saveToFile("URLs.txt", "a", original_url, "Original URL")
-    saveToFile("URLs.txt", "a", title, "URL Title")
+    saveToFile(filename="URLs.txt", mode="w", data=shorten_url, message="Shorten URL")
+    saveToFile(filename="URLs.txt", mode="a", data=original_url, message="Original URL")
+    saveToFile(filename="URLs.txt", mode="a", data=title, message="URL Title")
 
     # check the URLs.txt
     print(f"\n{colors.brown}Check the URLs.txt file for the shorten url, the original url and URL Title{colors.reset}")
